f2f4c3a96721c596bc4e6b8fb508812405ecd9ff
SajonasJ <jonas.sajonas@gmail.com>
2024-10-19 16:00:32 +1000

    removed header

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 
-import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
 import Section from "../components/Section.jsx";
 import Navigation from "../components/Navigation.jsx";
@@ -7,7 +6,6 @@ import Navigation from "../components/Navigation.jsx";
 export default function Home() {
   return (
     <main>
-      <Header />
       <Navigation />
       <Section />
       <Footer />
